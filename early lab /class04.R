895d1a08729409004af2a96d69730edc691796e0
Eddie Cintora <ecintora@ucsd.edu>
2026-05-24 15:26:07 -0700

    Add previous lab files

---
+++ early lab /class04.R
@@ -0,0 +1,8 @@
+#' ---
+#' title: "BIMM 143 Class 4"
+#' author: "Mason Dinh (A19140455)"
+#' date: "April 9th, 2026"
+#' ---
+# This is just a text file
+x <- 1:50
+plot(x, sin(x), typ="l", col="purple", lwd=3)
